4c6de2f293d4dce4e123a506a0cda3ea1b2626f6
Saurabh Verma <saurabhshalu@gmail.com>
2020-06-28 15:01:09 +0530

    ssh

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,14 +1,8 @@
 import 'dart:io';
 
 import 'package:CleanStreets/widgets/request-list.dart';
-import 'package:CleanStreets/widgets/request-tile.dart';
-
-import '../models/shared-data.dart';
-import 'package:CleanStreets/widgets/tasks-list.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-
-
 
 class HomeScreen extends StatefulWidget {
   static const String id = 'home_screen';
@@ -58,47 +52,3 @@ class _HomeScreenState extends State<HomeScreen>{
     );
   }
 }
-
-/*
- '${Provider.of<TaskData>(context).taskCount} Task',
-
-
-
- decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(20.0),
-                  topRight: Radius.circular(20.0),
-                ),
-              ),
-
-
-
-
-_image == null
-                ? Text('No image selected.')
-                : Image.file(_image),
-
-
-                ListView.builder(
-              itemCount: x.length,
-              itemBuilder: (BuildContext c, int i) {
-                if(i.isOdd) return Divider();
-                final index = i ~/ 2 + 1;
-                return _buildRow(index);
-              },
-            ),
-
-              Widget _buildRow(int idx) {
-    return ListTile(
-      leading: CircleAvatar(
-        child: Text('$idx'),
-      ),
-      title: Text(
-        'Item $idx',
-      ),
-
-    );
-  }
-
- */
