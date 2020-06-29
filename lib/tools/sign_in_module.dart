b0bd40e21d20d4455e9995c19dd9585d690914f9
Saurabh Verma <saurabhshalu@gmail.com>
2020-06-29 12:37:00 +0530

    log

--- lib/tools/sign_in_module.dart
+++ lib/tools/sign_in_module.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/services.dart';
-
 import '../models/shared-data.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
